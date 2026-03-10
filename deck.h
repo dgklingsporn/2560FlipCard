f7c72f7b4d7406e7964a649eb0612bdb5924020d
literally08 <logangreenwood27@gmail.com>
2026-03-10 15:12:50 -0400

    Add files via upload

--- deck.h
+++ deck.h
@@ -1,3 +1,11 @@
+/*
+Flip card part b
+EECE 2560
+3/10/26
+Group ID + #: ROBKLISYGRE 10
+deck.h
+This file declares the node structure and deck class used for the card deck
+*/
 #ifndef deck_h
 #define deck_h
 
@@ -16,13 +24,10 @@ struct node {
     node(T val, node<T>* n = nullptr) : nodeValue(val), next(n) {}
 };
 
-
-
-
 class deck
 {
 public:
-    deck();                         // constructor 
+    deck();                         // constructor
     ~deck();                        // destructor
 
     void shuffle();                 // shuffle deck
@@ -37,197 +42,7 @@ public:
     friend ostream& operator<<(ostream& ostr, const deck& d);
 
 private:
-    node<card>* firstCard = nullptr;
+    node<card>* firstCard;
 };
-// ----------------------------------------------
-// This creates 52 cards in order: Ace-King, then Club-Diamond-Heart-Spade
-deck::deck()
-{
-    firstCard = nullptr;
-    node<card>* tail = nullptr;
-
-    // Outer loop: Suits (1: Club, 2: Diamond, 3: Heart, 4: Spade)
-    for (int s = 0; s <= 3; s++)
-    {
-        // Inner loop: Values (1: Ace ... 13: King)
-        for (int v = 1; v <= 13; v++)
-        {
-            card newCard(s, v); // Assumes Member 1's constructor is Card(value, suit)
-            node<card>* newNode = new node<card>(newCard);
-
-            if (firstCard == nullptr)
-            {
-                firstCard = newNode;
-                tail = firstCard;
-            }
-            else
-            {
-                tail->next = newNode;
-                tail = newNode;
-            }
-        }
-    }
-}
-
-// ----------------------------------------------
-// clear decks previous memory
-void deck::clear()
-{
-    node<card>* curr = firstCard;
-
-    while (curr != nullptr)
-    {
-        node<card>* nextNode = curr->next;
-        delete curr;
-        curr = nextNode;
-    }
-
-    firstCard = nullptr;
-}
-
-
-//--------------------------
-// the destructor
-
-deck::~deck()
-{
-    clear();
-}
-
-
-//--------------------------
-// returns size of deck for confirming that we didnt kosing anything
-
-int deck::size() const
-{
-    int count = 0;
-    node<card>* curr = firstCard;
-
-    while (curr != nullptr)
-    {
-        count++;
-        curr = curr->next;
-    }
-
-    return count;
-}
-
-
-//--------------------------
-// checks if empty
-
-bool deck::isEmpty() const
-{
-    return firstCard == nullptr;
-}
-
-
-//--------------------------
-// The big one (shuffle method)
-
-void deck::shuffle()
-{
-    if (firstCard == nullptr || firstCard->next == nullptr)
-        return;
-
-    vector<card> temp;
-
-    // copy linked list → vector
-    node<card>* curr = firstCard;
-    while (curr != nullptr)
-    {
-        temp.push_back(curr->nodeValue);
-        curr = curr->next;
-    }
-
-    // random shuffle
-    random_device rd;
-    mt19937 gen(rd());
-    std::shuffle(temp.begin(), temp.end(), gen);
-
-    // delete old list
-    clear();
-
-    // rebuild list
-    firstCard = new node<card>(temp[0], nullptr);
-    node<card>* tail = firstCard;
-
-    for (size_t i = 1; i < temp.size(); i++)
-    {
-        tail->next = new node<card>(temp[i], nullptr);
-        tail = tail->next;
-    }
-}
-
-
-//--------------------------
-// get first card
-node<card>* deck::getFirst()
-{
-    return firstCard;
-}
-
-// deal out the first card
-node<card>* deck::deal()
-{
-    if (firstCard == nullptr)
-        return nullptr;
-
-    node<card>* dealtCard = firstCard;
-    firstCard = firstCard->next;
-    dealtCard->next = nullptr;
-
-    return dealtCard;
-}
-// place a passed card node on the bottom of the deck
-void deck::replace(node<card>* passedNode)
-{
-    if (passedNode == nullptr)
-        return;
-
-    passedNode->next = nullptr;
-
-    if (firstCard == nullptr)
-    {
-        firstCard = passedNode;
-        return;
-    }
-
-    node<card>* curr = firstCard;
-    while (curr->next != nullptr)
-    {
-        curr = curr->next;
-    }
-
-    curr->next = passedNode;
-}
-
-//--------------------------
-// print the deck
-ostream& operator<<(ostream& ostr, const deck& d)
-{
-    node<card>* curr = d.firstCard;
-    int count = 0;
-
-    ostr << "\nDeck:\n";
-
-    while (curr != nullptr)
-    {
-        count++;
-        ostr << curr->nodeValue;
-
-        if (curr->next != nullptr)
-            ostr << " ";
-
-        if (count % 13 == 0)
-            ostr << "\n";
-
-        curr = curr->next;
-    }
-
-    ostr << "\n";
-    return ostr;
-}
-
 
 #endif
