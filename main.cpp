15c6aa8ecf937799d02c52a880c021f10f2448b0
literally08 <logangreenwood27@gmail.com>
2026-03-10 15:11:19 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+/*
+Flip card part b
+EECE 2560
+3/10/26
+Group ID + #: ROBKLISYGRE 10
+main.cpp
+This is the main file for the game it
+connects the deck and card classes
+and controls the overall program
+*/
 #include "deck.h"
 #include <iostream>
 using namespace std;
