cfafe593256f0d1ea36d5cd92bc69d032df448a2
Alyssa-Roberts <roberts.aly@northeastern.edu>
2026-03-10 18:27:05 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -41,32 +41,68 @@ void playFlip()
   //(5) start the game and print the results
   cout << "Flip game started..." << endl;
 
-  int score = 0;
+  int score = 0; //keeping player's score
   int flipNumber = 1;
+  bool playerWantsToPlay = true;
 
-  while (!hand.isEmpty())
+  while (playerWantsToPlay && !hand.isEmpty()) //player wants to play and deck is not empty
   {
     node<card>* flipped = hand.deal();
-    cout << "Flip " << flipNumber << ": " << flipped->nodeValue << endl;
-    int value = flipped->nodeValue.getValue();
+    cout << "Flip " << flipNumber << ": " << flipped->nodeValue << endl; //flips card
+    int value = flipped->nodeValue.getValue(); //value
+    int suit = flipped->nodeValue.getSuit(); //suit
 
-    
-      
+    if(suit == "Heart") //extra point for heart
+    {
+      score += 1;
+    }
+
+    //points for each card value
+    if (value == "Ace")
+    {
+      score += 10;
+    }
+    else if (value == "King" || value == "Queen" || value == "Jack")
+    {
+      score += 5;
+    }
+    else if (value == "8" || value == "9" || value == "10")
+    {
+      score += 0; //receives 0 points
+    }
+    else if (value == "7")
+    {
+      score /= 2;
+    }
+    else if (value == "2" || value == "3" || value == "4" || value == "5" || value == "6")
+    {
+      score = 0;
+    }
+
+    delete flipped; //finish flip turn
+    flipNumber++; 
+
+    float playerChoice; //ask player if they want to continue to play
+    cout << "Would you like to flip another card? (y/n): ";
+    cin >> playerChoice;
+
+    if (playerChoice == 'y' || playerChoice == 'yes' || playerChoice == 'Y' || playerChoice == 'Yes')
+    {
+      playerWantsToPlay = true;
+    }
+    else
+    {
+      playerWantsToPlay = false; //doesn't want to continue to play
+    }
   }
-      
-  
+  //game has finished
+  cout << "Game Over" << endl;
+  cout << "Final Score: " << endl;
 }
 
 int main()
 {
-  deck d;
-  cout << "Before Shuffle:" << endl;
-  cout << d << endl;
-
-  d.shuffle();
-
-  cout << "After Shuffle:" << endl;
-  cout << d << endl;
+  playFlip(); //play global func
   
   return 0;
 }
